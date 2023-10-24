8db70274eaa7108b8b026fa4ad24473d71af3216
Leon <leondevlifelog@gmail.com>
2023-10-24 10:03:15 +0800

    fix: incorrect use of global issue number

--- src/main/kotlin/com/github/leondevlifelog/gitea/tasks/GiteaTask.kt
+++ src/main/kotlin/com/github/leondevlifelog/gitea/tasks/GiteaTask.kt
@@ -7,6 +7,19 @@ package com.github.leondevlifelog.gitea.tasks
 
 import com.intellij.tasks.generic.GenericTask
 import org.gitnex.tea4j.v2.models.Issue
+import java.util.*
 
 class GiteaTask(private val giteaRepository: GiteaRepository, private val issue: Issue) :
-    GenericTask(issue.id.toString(), issue.title, giteaRepository) {}
+    GenericTask(issue.number.toString(), issue.title, giteaRepository) {
+    override fun getCreated(): Date {
+        return issue.createdAt
+    }
+
+    override fun getDescription(): String? {
+        return issue.body
+    }
+
+    override fun getIssueUrl(): String? {
+        return issue.htmlUrl
+    }
+}
