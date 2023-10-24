f210858e38f48516c6e12d764c2455b3dd2820d0
Leon <leondevlifelog@gmail.com>
2023-10-24 10:03:37 +0800

    feat: support issue/task state update

--- src/main/kotlin/com/github/leondevlifelog/gitea/tasks/GiteaRepository.kt
+++ src/main/kotlin/com/github/leondevlifelog/gitea/tasks/GiteaRepository.kt
@@ -7,19 +7,24 @@ package com.github.leondevlifelog.gitea.tasks
 
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
+import com.intellij.openapi.diagnostic.Logger
 import com.intellij.openapi.util.text.StringUtil
+import com.intellij.tasks.CustomTaskState
 import com.intellij.tasks.LocalTask
 import com.intellij.tasks.Task
 import com.intellij.tasks.TaskRepositoryType
+import com.intellij.tasks.gitlab.GitlabRepository
 import com.intellij.tasks.impl.BaseRepository
 import com.intellij.tasks.impl.httpclient.NewBaseRepositoryImpl
 import com.intellij.util.xmlb.annotations.Tag
 import org.apache.http.client.methods.HttpGet
+import org.apache.http.client.methods.HttpPatch
 import org.apache.http.client.methods.HttpPost
 import org.apache.http.client.utils.URIBuilder
 import org.apache.http.entity.ContentType
 import org.apache.http.entity.StringEntity
 import org.gitnex.tea4j.v2.models.AddTimeOption
+import org.gitnex.tea4j.v2.models.EditIssueOption
 import org.gitnex.tea4j.v2.models.Issue
 import org.gitnex.tea4j.v2.models.TrackedTime
 import java.util.*
@@ -40,9 +45,11 @@ class GiteaRepository : NewBaseRepositoryImpl {
 
     var skipSSLVerify = false
 
+
     companion object {
         private val GSON: Gson = TaskGsonUtil.createDefaultBuilder().create()
         private val LIST_OF_ISSUES_TYPE: TypeToken<List<Issue>> = object : TypeToken<List<Issue>>() {}
+        private val LOG = Logger.getInstance(GitlabRepository::class.java)
     }
 
     constructor() : super() {
@@ -122,15 +129,42 @@ class GiteaRepository : NewBaseRepositoryImpl {
         addTimeOption.userName = username
         val matcher = TIME_SPENT_PATTERN.matcher(timeSpent)
         if (!matcher.find()) {
+            LOG.warn("can not get the time spent")
             return
         }
         val hour = matcher.group(0).toLong()
         val minute = matcher.group(1).toLong()
         val spendTimeInSecond = TimeUnit.HOURS.toSeconds(hour) + TimeUnit.MINUTES.toSeconds(minute)
         addTimeOption.time = spendTimeInSecond
+        LOG.debug("the time spend for issue:${task} is ${spendTimeInSecond}s")
         httpPost.entity = StringEntity(GSON.toJson(addTimeOption), ContentType.APPLICATION_JSON)
         val trackedTime: TrackedTime? =
             httpClient.execute(httpPost, TaskResponseUtil.GsonSingleObjectDeserializer(GSON, TrackedTime::class.java))
+        LOG.debug("response is $trackedTime")
         trackedTime ?: return
+    }
+
+    override fun setTaskState(task: Task, state: CustomTaskState) {
+        val updateTaskState = getRestApiUrl("repos", "$repoAuthor", "$repoName", "issues", task.id)
+        val url = URIBuilder(updateTaskState).addParameter("access_token", password).build()
+        val httpPatch = HttpPatch(url)
+        val editIssueOption = EditIssueOption()
+        editIssueOption.state = state.id
+        httpPatch.entity = StringEntity(GSON.toJson(editIssueOption), ContentType.APPLICATION_JSON)
+        val issue =
+            httpClient.execute(httpPatch, TaskResponseUtil.GsonSingleObjectDeserializer(GSON, Issue::class.java))
+        issue ?: return
+    }
+
+    override fun getAvailableTaskStates(task: Task): MutableSet<CustomTaskState> {
+        return mutableSetOf(CustomTaskState("open", "Open"), CustomTaskState("closed", "Close"))
+    }
+
+    override fun getFeatures(): Int {
+        return super.getFeatures() or TIME_MANAGEMENT or STATE_UPDATING
+    }
+
+    override fun isUseHttpAuthentication(): Boolean {
+        return true
     }
 }
